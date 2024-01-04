34ee2e0a46567b2bcc2bdedb809a332eca4d891e
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2024-01-04 16:23:43 +0000

    Terminado

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 
-//include images into your bundle
 
-
-//create your first component
 const TrafficLight = () => {
 	
     const [color, setColor] = useState("red");
-    
+    const [verPurple, setVerPurple] = useState(false);
 
     const HandleClick = (color) => {
         setColor(color);
+        setVerPurple(false);
+    };
+
+    const ChangeColor = () => {
+        if (color === "red") {
+            setColor("yellow");
+        } else if (color === "yellow") {
+            setColor("green");
+        } else if (color === "green") {
+            setColor(verPurple ? "purple" : "red");
+        } else if (color === "purple") {
+            setColor("red"); 
+        }
+    };
+
+    const AlternarPurple = () => {
+        setVerPurple(!verPurple); 
     };
 
     return (
@@ -27,9 +41,17 @@ const TrafficLight = () => {
                 className={`green ${color === "green" ? "selected" : ""}`} 
                 onClick={() => HandleClick("green")}
             ></div>
-			
+            {verPurple && (
+                <div 
+                    className={`purple ${color === "purple" ? "selected" : ""}`} 
+                    onClick={() => HandleClick("purple")}
+                ></div>
+            )}
+            <button id= "purpleBoton" onClick={AlternarPurple}>Púrpura</button> <br/>
+			 <button id= "colorBoton" onClick={ChangeColor}>Color</button>
         </div>
+        
     );
 };
 
-export default TrafficLight;
+export default TrafficLight
